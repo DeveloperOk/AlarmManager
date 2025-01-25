c6e4783d58588f00aefda6a6148f3194993d6ccc
DeveloperOk <ok.fahrettin@gmail.com>
2025-01-25 15:37:30 +0300

    scheduleExactAndAllowWhileIdle feature was added.

--- app/src/main/java/com/enterprise/alarmmanager/AlarmScheduler.kt
+++ app/src/main/java/com/enterprise/alarmmanager/AlarmScheduler.kt
@@ -1,12 +1,17 @@
 package com.enterprise.alarmmanager
 
 import android.app.PendingIntent
+import android.content.Context
 
 
 interface AlarmScheduler {
     fun createPendingIntent(reminderItem: ReminderItem): PendingIntent
 
     fun schedule(reminderItem: ReminderItem)
+
+    fun scheduleExactAndAllowWhileIdle(reminderItem: ReminderItem)
+
+    fun requestPermission(context: Context)
 
     fun cancel(reminderItem: ReminderItem)
 }
--- app/src/main/java/com/enterprise/alarmmanager/AppAlarmScheduler.kt
+++ app/src/main/java/com/enterprise/alarmmanager/AppAlarmScheduler.kt
@@ -4,6 +4,9 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.provider.Settings
+import androidx.core.content.ContextCompat
 
 
 class AppAlarmScheduler(
@@ -30,6 +33,32 @@ class AppAlarmScheduler(
             AlarmManager.INTERVAL_DAY,
             createPendingIntent(reminderItem)
         )
+    }
+
+
+    //This is not repeating, to achieve repeating, schedule one more with this
+    //function inside broadcast receiver thanks to that every broadcast receiver call will trigger one more schedule
+    //so that we will achieve repeating behavior
+    override fun scheduleExactAndAllowWhileIdle(reminderItem: ReminderItem) {
+        alarmManager.setExactAndAllowWhileIdle(
+            AlarmManager.RTC_WAKEUP,
+            reminderItem.time,
+            createPendingIntent(reminderItem)
+        )
+    }
+
+    override fun requestPermission(context: Context) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val alarmManager = ContextCompat.getSystemService(context, AlarmManager::class.java)
+            if (alarmManager?.canScheduleExactAlarms() == false) {
+                Intent().also { intent ->
+                    intent.action = Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM
+                    context.startActivity(intent)
+                }
+            }
+        }
+
     }
 
     override fun cancel(reminderItem: ReminderItem) {
